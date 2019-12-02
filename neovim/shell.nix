bce8a58680f6a58ccf0e63f98438893e1feab741
samrose <samuel.rose@gmail.com>
2019-12-02 14:23:41 -0500

    neovim: add alchemist support

--- neovim/shell.nix
+++ neovim/shell.nix
@@ -3,16 +3,23 @@ with pkgs;
 
 let
   inherit (perlPackages) vidir;
+  elixir-erlang-src = runCommand "elixir-erlang-src" {} ''
+    mkdir $out
+    ln -s ${elixir.src} $out/elixir
+    ln -s ${erlang.src} $out/otp
+  '';
   neovim-with-config = neovim.override {
     configure = {
       customRC = ''
         let g:deoplete#enable_at_startup = 1
         let g:deoplete#sources#rust#racer_binary='${rustracer}/bin/racer'
         let g:deoplete#sources#rust#rust_source_path='/home/samrose/Desktop/rust/src'
+        let g:alchemist#elixir_erlang_src = "${elixir-erlang-src}"
         set title
         set nu
       '';
       packages.package.start = with vimPlugins; [
+        alchemist-vim
         deoplete-nvim
         deoplete-jedi
         deoplete-go
@@ -45,6 +52,8 @@ mkShell {
     gocode
     go2nix
     neovim-with-config
+    python
+    python3
     rustc
     rustracer
     which
